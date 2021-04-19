e795fe3d2e9cd3cbecca7a3a1edec0bfc38fc5da
julianoleo <50123832+julianoleo@users.noreply.github.com>
2021-04-19 20:24:46 -0300

    Cifra Vegenere - IFTM - 2021

---
+++ Form_CifraVigenere.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CifraVigenere
+{
+    partial class Form_CifraVigenere
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMsgOriginal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChave = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMsgCriptografada = new System.Windows.Forms.TextBox();
+            this.btnCriptografar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMsgOriginal
+            // 
+            this.txtMsgOriginal.Location = new System.Drawing.Point(7, 24);
+            this.txtMsgOriginal.Multiline = true;
+            this.txtMsgOriginal.Name = "txtMsgOriginal";
+            this.txtMsgOriginal.Size = new System.Drawing.Size(252, 77);
+            this.txtMsgOriginal.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mensagem";
+            // 
+            // txtChave
+            // 
+            this.txtChave.Location = new System.Drawing.Point(7, 125);
+            this.txtChave.Multiline = true;
+            this.txtChave.Name = "txtChave";
+            this.txtChave.Size = new System.Drawing.Size(252, 28);
+            this.txtChave.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chave";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mensagem Criptografada";
+            // 
+            // txtMsgCriptografada
+            // 
+            this.txtMsgCriptografada.Location = new System.Drawing.Point(7, 181);
+            this.txtMsgCriptografada.Multiline = true;
+            this.txtMsgCriptografada.Name = "txtMsgCriptografada";
+            this.txtMsgCriptografada.Size = new System.Drawing.Size(252, 77);
+            this.txtMsgCriptografada.TabIndex = 4;
+            // 
+            // btnCriptografar
+            // 
+            this.btnCriptografar.Location = new System.Drawing.Point(159, 264);
+            this.btnCriptografar.Name = "btnCriptografar";
+            this.btnCriptografar.Size = new System.Drawing.Size(100, 23);
+            this.btnCriptografar.TabIndex = 6;
+            this.btnCriptografar.Text = "Criptografar";
+            this.btnCriptografar.UseVisualStyleBackColor = true;
+            this.btnCriptografar.Click += new System.EventHandler(this.btnCriptografar_Click);
+            // 
+            // Form_CifraVigenere
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 293);
+            this.Controls.Add(this.btnCriptografar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMsgCriptografada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsgOriginal);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_CifraVigenere";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cifra de Vigenere - IFTM 2021";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsgOriginal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMsgCriptografada;
+        private System.Windows.Forms.Button btnCriptografar;
+    }
+}
+
---
+++ Form_CifraVigenere.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CifraVigenere
+{
+    public partial class Form_CifraVigenere : Form
+    {
+        public Form_CifraVigenere()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static string CifraVigenere(string mensagem, string chave, bool flag)
+        {
+            if(flag)
+            {
+                var codigo = "";
+                for (int i = 0, j = 0; i < mensagem.Length; i++, j++) {
+                    char c = char.ToUpper(mensagem[i]);
+                    if (c < 'A' || c > 'Z') continue;
+                    codigo += (char)((c + char.ToUpper(chave[j % chave.Length]) - 2 * 'A') % 26 + 'A');
+                }
+                return codigo;
+            }
+            return "";
+        }
+
+        private void btnCriptografar_Click(object sender, EventArgs e)
+        {
+            if(txtMsgOriginal.Text == string.Empty || txtChave.Text == string.Empty)
+            {
+                MessageBox.Show("Favor informar uma mensagem ou uma chave válida!");
+            }
+            else
+            {
+                string textoOriginal = txtMsgOriginal.Text.ToUpper();
+                string chave = txtChave.Text.ToUpper();
+                string resultado = CifraVigenere(textoOriginal, chave, true);
+                txtMsgCriptografada.Text = string.Empty;
+                txtMsgCriptografada.Text = resultado;
+            }
+        }
+    }
+}
